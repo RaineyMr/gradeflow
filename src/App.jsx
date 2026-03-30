3e365b187509697c03688e5f8108f5dea8ad825e
RaineyMr <hire.mr.rainey@gmail.com>
2026-03-29 22:53:08 -0500

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ import Tutorials       from './pages/Tutorials'
 import Widgets         from './pages/Widgets'
 import ProfileSettings from './components/ProfileSettings'
 import SupportStaffDashboard from './pages/SupportStaffDashboard'
+import StudentTrends from './pages/StudentTrends'
 
 // ─────────────────────────────────────────────────────────────────────────────
 // Helpers
