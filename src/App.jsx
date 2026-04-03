5e9dd3d7cc3994c3c2ea7d9f5247d3c2767dedad
RaineyMr <hire.mr.rainey@gmail.com>
2026-04-03 14:09:37 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import ProtectedRoute from './router/ProtectedRoute'
 // ── Pages ─────────────────────────────────────────────────────────────────────
 import Login from '@pages/Login'
 import TeacherOnboarding from '@pages/TeacherOnboarding'
+import CurriculumOnboarding from '@pages/CurriculumOnboarding'
 import WorkingDashboard from '@pages/Dashboard_Working'
 import AdminDashboard from '@pages/AdminDashboard'
 import Dashboard from '@pages/Dashboard'
