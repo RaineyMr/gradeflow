949b3dafba5f7dd8cc9294bdb19701cffe35c1c6
RaineyMr <hire.mr.rainey@gmail.com>
2026-04-03 15:27:37 -0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import AppRouter from './appRouter.jsx'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="*" element={<AppRouter />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path="*" element={<AppRouter />} />
+      </Routes>
+      <SpeedInsights />
+    </>
   )
 }
