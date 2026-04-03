ea54b0a1a86ce5d6dea69d402dafe5eb1cf49a51
RaineyMr <hire.mr.rainey@gmail.com>
2026-04-03 15:12:02 -0500

    update

--- src/appRouter.jsx
+++ src/appRouter.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import { SpeedInsights } from '@vercel/speed-insights/react'
 import { useStore } from './lib/store'
 import Dashboard from './pages/Dashboard'
 import Gradebook from './pages/Gradebook'
