4dffd3852ea73629c6423596dedc2c8c985c39a3
RaineyMr <hire.mr.rainey@gmail.com>
2026-04-11 21:16:51 -0500

    update

--- src/pages/LessonCalendar.jsx
+++ src/pages/LessonCalendar.jsx
@@ -469,7 +469,7 @@ export default function LessonCalendar({ onBack }) {
     const lessonDate = new Date(lesson.date).toISOString().split('T')[0]
     const url = `#/teacher/lessons?date=${lessonDate}&mode=edit&lessonId=${lesson.id}`
     console.log('Navigating to:', url)
-    window.location.hash = url
+    window.location.assign(url)
   }
 
   return (
